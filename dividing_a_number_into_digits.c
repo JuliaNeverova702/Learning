57a4505101f48a08cf414ae446b50557c1034ae7
Julia Neverova <julia.neverova@list.ru>
2022-02-08 23:09:25 +0700

    Dividing a number into digits

--- dividing_a_number_into_digits.c
+++ dividing_a_number_into_digits.c
@@ -4,18 +4,27 @@
 
 int main() {
     int number;
-    char trash;
-    printf("Enter a 5-digit number: ");
-    scanf("%d%c", &number, &trash);
+    printf("Enter a five-digit number: ");
+    scanf("%d", &number);
+    printf("\v");
+    
+    if ((number < 10000) || (number >99999)){
+        printf("Error. Try again.\n"); 
+        return 0;
+    }
     
     int one, two, three, four, five;
+    one = (number / 10000) % 10;
+    two = (number / 1000) % 10;
+    three = (number / 100) % 10;
+    four = (number / 10) % 10;
     five = number % 10;
     
-    printf("%d", five);  
-      
-    if (trash =! "/t"){
-        printf ("n/a");
-    }
+    printf("1st number is %d\n", one);
+    printf("2nd number is %d\n", two);
+    printf("3rd number is %d\n", three);
+    printf("1st number is %d\n", four);
+    printf("1st number is %d\n", five); 
     
     return 0;
 }
